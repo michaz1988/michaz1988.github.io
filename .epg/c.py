c46ea1f84f44b21ccc34cf9f5a6d99761660f739
michaz1988 <michaz1988@gmail.com>
2024-12-02 21:22:15 +0100

    02.Dec.2024 21:22:15

--- .epg/c.py
+++ .epg/c.py
@@ -1,8 +1,9 @@
-import requests, json, os, gzip
+import requests, os, gzip
 from datetime import datetime, timedelta
 from concurrent.futures import ThreadPoolExecutor, as_completed
 
 today = datetime.today()
+now = datetime.now()
 
 tvsDE_header = {'Host': 'live.tvspielfilm.de',
 	'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:75.0) Gecko/20100101 Firefox/75.0',
@@ -12,8 +13,30 @@ tvsDE_header = {'Host': 'live.tvspielfilm.de',
 	'Connection': 'keep-alive',
 	'Upgrade-Insecure-Requests': '1'}
 
+magentaDE_login_url = 'https://api.prod.sngtv.magentatv.de/EPG/JSON/Login?&T=PC_firefox_75'
+magentaDE_authenticate_url = 'https://api.prod.sngtv.magentatv.de/EPG/JSON/Authenticate?SID=firstup&T=PC_firefox_75'
+magentaDE_channellist_url = 'https://api.prod.sngtv.magentatv.de/EPG/JSON/AllChannel?SID=first&T=PC_firefox_75'
+magentaDE_data_url = 'https://api.prod.sngtv.magentatv.de/EPG/JSON/PlayBillList?userContentFilter=241221015&sessionArea=1&SID=ottall&T=PC_firefox_75'
+magentaDE_login = {'userId': 'Guest', 'mac': '00:00:00:00:00:00'}
+magentaDE_authenticate = {'terminalid': '00:00:00:00:00:00', 'mac': '00:00:00:00:00:00', 'terminaltype': 'WEBTV','utcEnable': '1', 'timezone': 'UTC', 'userType': '3', 'terminalvendor': 'Unknown','preSharedKeyID': 'PC01P00002', 'cnonce': '5c6ff0b9e4e5efb1498e7eaa8f54d9fb'}
+magentaDE_get_chlist = {'properties': [{'name': 'logicalChannel','include': '/channellist/logicalChannel/contentId,/channellist/logicalChannel/name,/channellist/logicalChannel/pictures/picture/imageType,/channellist/logicalChannel/pictures/picture/href'}],'metaDataVer': 'Channel/1.1', 'channelNamespace': '2','filterlist': [{'key': 'IsHide', 'value': '-1'}], 'returnSatChannel': '0'}
+magentaDE_header = {'Host': 'api.prod.sngtv.magentatv.de',
+				  'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:75.0) Gecko/20100101 Firefox/75.0',
+				  'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+				  'Accept-Language': 'de,en-US;q=0.7,en;q=0.3',
+				  'Accept-Encoding': 'gzip, deflate, br',
+				  'Connection': 'keep-alive',
+				  'Upgrade-Insecure-Requests': '1'}
+
+def magentaSession():
+	session = requests.Session()
+	session.post(magentaDE_login_url, json=magentaDE_login, headers=magentaDE_header)
+	session.post(magentaDE_authenticate_url, json=magentaDE_authenticate, headers=magentaDE_header)
+	session.headers.update({'X_CSRFToken': session.cookies["CSRFSESSION"]})
+	return session
+
+magentacontentIDs = ["148"]
 contentIDs = ['ARD', 'ZDF', 'RTL', 'SAT1', 'PRO7', 'K1', 'RTL2', 'VOX', '3SAT', 'ARTE', 'SERVUSA', 'TELE5', 'SPTVW', 'JUKE', 'HEIMA', 'DMAX', 'SIXX', 'RTL-N', 'RTLPL', 'SPO-D', 'SAT1G', 'PRO7M', 'CC', 'WDR', 'N3', 'BR', 'SWR', 'HR', 'MDR', 'RBB', 'SPORT', 'S1PLU', 'EURO', 'EURO2', 'AMS', 'KIKA', 'SUPER', 'TOGGO', 'RIC', 'NICK', 'FFTV', 'NICKJ', 'NICKT', 'PHOEN', 'ALPHA', 'FES', '2NEO', 'ZINFO', 'ANIXE', 'TLC', 'WDWTV', 'VOXUP', 'TAG24', 'NTV', 'WELT', 'N24DOKU', 'K1DOKU', 'DMC', 'MTV', '123TV', 'ATV', 'ATV2', 'ORF1', 'ORF2', 'ORF3', 'ORFSP', 'OE24TV', 'PULS4', 'SF1', 'SF2', 'CIN', 'SKY-F', 'SKY-A', 'SKY-N', 'SKYCH', 'SKY-CR', 'SKY-D', 'SKY-NA', 'TNT-F', 'KINOW', 'SKY1', 'SKYAT', 'SKY-K', 'SKYRP', 'UNIVE', 'HISHD', 'SP-GE', 'MOVTV', 'HDDIS', 'N-GHD', 'N-GW', '13TH', 'SCIFI', 'TNT-S', 'TNT-C', 'CRIN', 'ROM', 'C-NET', 'CLASS', 'APLAN', 'AXN', 'GEO', 'K1CLA', 'SAT1E', 'PRO7F', 'RTL-C', 'RTL-L', 'PASS', 'SILVE', 'SONY', 'SPTVW', 'TRACE']
-#contentIDs= ['ARD']
 provider = 'TV SPIELFILM (DE)'
 lang = 'de'
 enable_rating_mapper = True
@@ -21,8 +44,7 @@ days_to_grab = 2
 episode_format = "onscreen"
 channel_format = 'provider'
 genre_format = "provider"
-start = True
-now = datetime.now()
+
 addon_name = "Takealug EPG Grabber"
 addon_version = "1.1.7"
 
@@ -36,11 +58,7 @@ def xml_broadcast(episode_format, channel_id, item_title, item_starttime, item_e
             channel_id = channel_id.replace('&', '&amp;').replace('<', '&lt;').replace('>', '&gt;')
             guide.append('    <programme start="{} +0000" stop="{} +0000" channel="{}">\n'.format(item_starttime, item_endtime, channel_id))
 
-        ## Map Imdb Stars
-        if not item_starrating == '':
-            stars = mapper.map_stars(item_starrating)
-        else:
-            stars = ''
+        stars = ''
 
         ## TITLE Condition
         if not item_title == '':
@@ -236,11 +254,7 @@ def get_epg(tvs_data_url):
 			#channel_id = channel_id.replace('&', '&amp;').replace('<', '&lt;').replace('>', '&gt;')
 			guide.append('	<programme start="{} +0000" stop="{} +0000" channel="{}">\n'.format(item_starttime, item_endtime, channel_id))
 
-		## Map Imdb Stars
-		if not item_starrating == '':
-			stars = mapper.map_stars(item_starrating)
-		else:
-			stars = ''
+		stars = ''
 
 		## TITLE Condition
 		if not item_title == '':
@@ -417,18 +431,32 @@ for channel in tvsDE_chlist_url:
 		day_to_grab = day_to_start.strftime("%Y-%m-%d")
 		day_to_start += timedelta(days=1)
 		tvs_data_urls.append('{}/{}'.format(channel_id, day_to_grab))
+
+epg.append('\n<!--  {}  CHANNEL LIST -->\n'.format('MAGENTA TV (DE)'))
+magentaDE_channels = magentaSession().post(magentaDE_channellist_url, json=magentaDE_get_chlist,headers=magentaDE_header).json()
+for channels in magentaDE_channels["channellist"]:
+	channel_id = channels['contentId']
+	if channel_id not in magentacontentIDs: continue
+	channel_name = channels['name']
+	for image in channels['pictures']:
+		if image['imageType'] == '15':
+			channel_icon = image['href']
+	epg.append('    <channel id="{}">\n'.format(channel_id))
+	epg.append('        <display-name lang="{}">{}</display-name>\n'.format(lang, channel_name))
+	epg.append('        <icon src="{}" />\n'.format(channel_icon))
+	epg.append('    </channel>\n')
 		
 epg.append('\n<!--  {}  PROGRAMME LIST -->'.format('ZAPPN'))
 api_headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/110.0', 'Content-type': 'application/json;charset=utf-8', 'X-Api-Date-Format': 'iso', 'X-Api-Camel-Case': 'true', 'referer': 'https://streaming.simplitv.at/'}
 data_headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/110.0', 'Accept': 'application/json'}
-time_start = str(datetime.now().strftime("%Y-%m-%dT%H:%M:00.000Z"))
-time_end = str((datetime.now() + timedelta(days=2)).strftime("%Y-%m-%dT%H:%M:00.000Z"))
+time_start = str(now.strftime("%Y-%m-%dT%H:%M:00.000Z"))
+time_end = str((now + timedelta(days=2)).strftime("%Y-%m-%dT%H:%M:00.000Z"))
 epg_url = "https://api.app.simplitv.at/v1/EpgTile/FilterProgramTiles?$headers=%7B%22Content-Type%22:%22application%2Fjson%3Bcharset%3Dutf-8%22,%22X-Api-Date-Format%22:%22iso%22,%22X-Api-Camel-Case%22:true%7D"
-epg_post = json.dumps({"platformCodename": "www", "from": time_start, "to": time_end})
-epg_resp = requests.post(epg_url, timeout=5, headers=api_headers, data=epg_post, allow_redirects=False).json()["programs"]
+epg_post = {"platformCodename": "www", "from": time_start, "to": time_end}
+epg_resp = requests.post(epg_url, timeout=5, headers=api_headers, json=epg_post, allow_redirects=False).json()["programs"]
 prg_url = "https://api.app.simplitv.at/v2/Tile/GetTiles?$headers=%7B%22Content-Type%22:%22application%2Fjson%3Bcharset%3Dutf-8%22,%22X-Api-Date-Format%22:%22iso%22,%22X-Api-Camel-Case%22:true%7D"
-prg_post = json.dumps({"platformCodename": "www", "requestedTiles": [{"id": a["id"]} for i in epg_resp.keys() for a in epg_resp[i] if i == "puls24"]})
-epg_data = requests.post(prg_url, timeout=5, headers=api_headers, data=prg_post, allow_redirects=False).json()["tiles"]
+prg_post = {"platformCodename": "www", "requestedTiles": [{"id": a["id"]} for i in epg_resp.keys() for a in epg_resp[i] if i == "puls24"]}
+epg_data = requests.post(prg_url, timeout=5, headers=api_headers, json=prg_post, allow_redirects=False).json()["tiles"]
 for program in epg_data:
 	item_starttime = datetime.strptime(program["start"].split('+')[0], '%Y-%m-%dT%H:%M:%S').strftime('%Y%m%d%H%M%S')
 	item_endtime = datetime.strptime(program["stop"].split('+')[0], '%Y-%m-%dT%H:%M:%S').strftime('%Y%m%d%H%M%S')
@@ -459,6 +487,58 @@ with ThreadPoolExecutor(len(tvs_data_urls)) as executor:
 	for future in as_completed(futures):
 		o = future.result()
 		epg.append(o)
+		
+epg.append('\n<!--  {}  PROGRAMME LIST -->'.format('MAGENTA TV (DE)'))
+starttime = datetime(today.year, today.month, today.day, hour=00, minute=00, second=1).strftime("%Y%m%d%H%M%S")
+calc_then = datetime(today.year, today.month, today.day, hour=23, minute=59, second=59)
+calc_then += timedelta(days=days_to_grab)
+endtime = calc_then.strftime("%Y%m%d%H%M%S")
+for contentID in magentacontentIDs:
+	magentaDE_data = {'channelid': contentID, 'type': '2', 'offset': '0', 'count': '-1', 'isFillProgram': '1','properties': '[{"name":"playbill","include":"ratingForeignsn,id,channelid,name,subName,starttime,endtime,cast,casts,country,producedate,ratingid,pictures,type,introduce,foreignsn,seriesID,genres,subNum,seasonNum"}]','endtime': endtime, 'begintime': starttime}
+	magentaData = magentaSession().post(magentaDE_data_url, json=magentaDE_data, headers=magentaDE_header).json()['playbilllist']
+	for playbilllist in magentaData:
+		item_title = playbilllist.get('name',"")
+		item_starttime = playbilllist.get('starttime', "")
+		item_endtime = playbilllist.get('endtime',"")
+		item_description = playbilllist.get('introduce',"")
+		item_country = playbilllist.get('country',"")
+		try:
+			item_picture = playbilllist['pictures'][1]['href']
+		except (KeyError, IndexError):
+			item_picture = ''
+		item_subtitle = playbilllist.get('subName',"")
+		items_genre = playbilllist.get('genres',"")
+		item_date = playbilllist.get('producedate',"")
+		item_season = playbilllist.get('seasonNum',"")
+		item_episode = playbilllist.get('subNum', "")
+		item_agerating = playbilllist.get('ratingid', "")
+		try:
+			items_director = playbilllist['cast']['director']
+		except (KeyError, IndexError):
+			items_director = ''
+		try:
+			items_producer = playbilllist['cast']['producer']
+		except (KeyError, IndexError):
+			items_producer = ''
+		try:
+			items_actor = playbilllist['cast']['actor']
+		except (KeyError, IndexError):
+			items_actor = ''
+		item_starrating = ''
+		if not item_date == '':
+			item_date = item_date.split('-')
+			item_date = item_date[0]
+		if (not item_starttime == '' and not item_endtime == ''):
+			start = item_starttime.split(' UTC')
+			item_starttime = start[0].replace(' ', '').replace('-', '').replace(':', '')
+			stop = item_endtime.split(' UTC')
+			item_endtime = stop[0].replace(' ', '').replace('-', '').replace(':', '')
+		if not item_country == '':
+			item_country = item_country.upper()
+		if item_agerating == '-1':
+			item_agerating = ''
+		epg.append(xml_broadcast(episode_format, contentID, item_title, item_starttime, item_endtime, item_description, item_country, item_picture, item_subtitle, items_genre, item_date, item_season, item_episode, item_agerating, item_starrating, items_director, items_producer, items_actor, enable_rating_mapper, lang))
+		
 epg.append('\n</tv>\n')
 
 
