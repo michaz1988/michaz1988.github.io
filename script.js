3735973efbfa6ed8cae1e02deaaeb30bc34425e2
michaz1988 <45913369+michaz1988@users.noreply.github.com>
2025-05-09 09:30:34 +0200

    update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+// @name Alert test
+// @description Displays an alert dialog on google.it
+// @author Emanuele Faranda
+// @version 1.0
+// @match *://www.google.it/*
+
 alert("[+] Arch:", Process.arch)
