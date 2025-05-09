0cdbc26eee09a0fbf914a2d06c6f8df5f7f503c4
michaz1988 <michaz1988@gmail.com>
2025-05-09 18:32:53 +0200

    09.May.2025 18:32:52

--- script.js
+++
@@ -1,7 +0,0 @@
-// @name Alert test
-// @description Displays an alert dialog on google.it
-// @author Emanuele Faranda
-// @version 1.0
-// @match *://www.google.it/*
-
-alert("[+] Arch:", Process.arch)
