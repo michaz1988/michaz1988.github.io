e95a1435d9d8e1300f4acb0da9011377786dd2ba
michaz1988 <michaz1988@gmail.com>
2026-03-05 21:10:37 +0100

    05.Mar.2026 21:10:37

--- .epg/maclist.py
+++ .epg/maclist.py
@@ -1,5 +1,4 @@
-{
-    "http://focus.viptveu.com:8080/c": [
+alllist = {"http://focus.viptveu.com:8080/c": [
         "00:1A:79:08:43:9B",
         "A0:BB:3E:B5:FA:09",
         "00:2A:01:92:0A:F5",
