1b1770be780752eaea41a4620d22fc4527e3d5a1
michaz1988 <michaz1988@users.noreply.github.com>
2026-08-24 20:55:30 +0200

    Tools: Speedtest-Import unter Kodi reparieren

--- plugin.video.tools/resources/lib/speedtest.py
+++ plugin.video.tools/resources/lib/speedtest.py
@@ -181,7 +181,7 @@ except ImportError:
     try:
         _py3_utf8_stdout = _Py3Utf8Output(sys.stdout)
         _py3_utf8_stderr = _Py3Utf8Output(sys.stderr)
-    except OSError:
+    except (AttributeError, OSError, TypeError, ValueError):
         # sys.stdout/sys.stderr is not a compatible stdout/stderr object
         # just use it and hope things go ok
         _py3_utf8_stdout = sys.stdout
